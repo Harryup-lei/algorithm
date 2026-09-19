2b82a60f8b8da12c9041ccdc59d6b94ba616729b
边城巷深 <2185833595@qq.com>
2026-09-19 13:56:08 +0800

    java算法 中位数贪心获取最小步骤数 Lettcode462最小操作次数使数组元素相等

---
+++ src/main/java/com/kelei/Lettcode462.java
@@ -0,0 +1,20 @@
+package com.kelei;
+
+public class Lettcode462 {
+	public int minMoves2(int[] nums) {
+		//先找数组数的中位数,在进行处理
+		int n = nums.length;
+		if(n == 1){
+			return 0;
+		}else if(n == 2){
+			return Math.max(nums[0], nums[1]) - Math.min(nums[0], nums[1]);
+		}
+		Arrays.sort(nums);
+		int middle = nums[n / 2];
+		int ans = 0;
+		for (int x : nums) {
+			ans += Math.abs(x - middle);
+		}
+		return ans;
+	}
+}
